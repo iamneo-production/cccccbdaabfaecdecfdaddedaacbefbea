b2412c5f2ddb28054f1e75275bca0bcf4638dd4b
Iamneo <techadmin@iamneo.ai>
2023-09-12 08:20:05 +0000

    Backup Commit

---
+++ DATEOFBIRTH.sh
@@ -0,0 +1,7 @@
+read -p "enter your birth day i.e DD " day
+echo   
+read -p "enter your birth month i.e MM " month
+echo   
+read -p "enter your birth year i.e YYYY " year
+echo   
+echo "$day-$month-$year"
--- Untitled-1.sh
+++
@@ -1,7 +0,0 @@
-read -p "enter your birth day " day
-echo   
-read -p "enter your birth month " month
-echo   
-read -p "enter your birth year " year
-echo   
-echo 
